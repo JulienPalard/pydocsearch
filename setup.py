77b062f7f9051498ec3e77989e67cce7664bdc56
Julien Palard <julien@palard.fr>
2016-05-10 22:09:23 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='pydocsearch',
-      version='1.0.0',
+      version='1.0.1',
       description='Search a page in docs.python.org',
       long_description=open('README.rst', 'rt').read(),
       author='Julien Palard',
       author_email='julien@palard.fr',
       keywords=['pydoc', 'docs.python.org', 'search'],
       url='https://github.com/JulienPalard/pydocsearch',
-      download_url='https://github.com/julienpalard/pydocsearch/tarball/1.0.0',
+      download_url='https://github.com/julienpalard/pydocsearch/tarball/1.0.1',
       py_modules=['pydocsearch'],
       classifiers=['Development Status :: 4 - Beta',
                    'Intended Audience :: Developers',
